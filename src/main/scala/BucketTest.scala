ebe759f4101c02be552e3cdf36f40bf212e8ce78
Jungho Choi <cjoongho@gmail.com>
2020-03-10 11:51:05 +0900

    [spark 2.4.0] Bucket pruning

---
+++ src/main/scala/BucketTest.scala
@@ -0,0 +1,36 @@
+import org.apache.spark.sql.catalyst.TableIdentifier
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
+/**
+ * spark 2.4.0 review - Bucket pruning
+ * https://www.waitingforcode.com/apache-spark-sql/apache-spark-2.4.0-features-bucket-pruning/read#bucket_pruning_implementation
+ */
+object BucketTest {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .appName("WindowTest")
+      .master("local[*]")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val tableName = s"order${System.currentTimeMillis()}"
+    val orders = Seq((1L, "user1"), (2L, "user2"), (3L, "user3"), (4L, "user1"), (5L, "user4"), (6L, "user5"))
+      .toDF("order_id", "user_id")
+
+    orders.write.mode(SaveMode.Overwrite).bucketBy(2, "user_id").saveAsTable(tableName)
+
+    val metadata = spark.sessionState.catalog.getTableMetadata(TableIdentifier(tableName))
+    println(metadata.bucketSpec)
+
+    val filteredBuckts = spark.sql(s"select * from ${tableName} where user_id = 'user1'")
+
+    println("========== executedPlan ==========")
+    println(filteredBuckts.queryExecution.executedPlan.toString())
+  }
+
+
+}
